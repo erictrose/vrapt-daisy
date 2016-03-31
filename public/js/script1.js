08384da5535f6e014e7aeb89f5b0bb7e9df5be68
gitlabadmin <erictrose@gmail.com>
2016-03-30 21:03:14 -0400

    added angular route and starting angular app

--- public/js/script1.js
+++ public/js/script1.js
@@ -1,1 +1,7 @@
 console.log('script 1 loaded');
+
+var app = angular.module("app", ["firebase","ngRoute"]);
+
+//all components loaded in html header
+
+console.log('app.js loaded');
--- public/js/script3.js
+++ public/js/script3.js
@@ -1,7 +1,9 @@
 console.log('script 3 loaded');
 
+//angular test
 if(typeof angular == 'undefined') {
     console.log('NO angular');
 } else {
     console.log('angular');
 }
+
