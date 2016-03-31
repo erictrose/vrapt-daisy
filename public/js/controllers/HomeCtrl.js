b8edc451ead8b448442801866ae7e6e7b0b50b16
gitlabadmin <erictrose@gmail.com>
2016-03-31 16:35:14 -0400

    added firebase rotation variable test

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,27 +1,56 @@
 app.controller("HomeCtrl", ["currentAuth", "$scope", "$rootScope", "$routeParams", "$location", "$firebaseArray", "$firebaseObject",  function(currentAuth, $scope, $rootScope, $routeParams, $location, $firebaseArray, $firebaseObject) {
     
+//    //connect to firebase directories
+//    var myFloorRef = new Firebase("https://vrapt.firebaseio.com/floors/" + currentAuth.uid);
+    
     //connect to firebase directories
-    var myFloorRef = new Firebase("https://vrapt.firebaseio.com/floors/" + currentAuth.uid);
+    var fbRef = new Firebase("https://vrapt.firebaseio.com/rotate");
+    var fbObj = new $firebaseObject(fbRef);
+    
+    fbObj.$loaded().then(function(){
+        console.log(fbObj.$value);
+        
+        $scope.test = fbObj.$value;
+        
+        //ROTATE RIGHT
+        $scope.rotateThingRight = function(){
+            fbObj.$value += 1;
+            fbObj.$save();
+            console.log('rotating right');
+            console.log(fbObj.$value);
+            $scope.test = fbObj.$value;
+        };
 
+        //ROTATE LEFT
+        $scope.rotateThingLeft = function(){
+            fbObj.$value -= 1;
+            fbObj.$save();
+            console.log('rotating left');
+            console.log(fbObj.$value);
+            $scope.test = fbObj.$value;
+        };
+        
+    });
+    
+
+//    //link to variables
+//    var myFloor = $firebaseArray(myFloorRef);
+    
     //link to variables
-    var myFloor = $firebaseArray(myFloorRef);
+
     
-    //link to scope
-    $scope.myFloor = myFloor;
     
-    //set scope variables
-    $scope.test = 'WORKING';
-    $scope.openAModalOrSomething = true;
+//    //link to scope
+//    $scope.myFloor = myFloor;
+    
+    
+    
+    
     
     //go back, needs to be a factory or service or something
     $scope.goTo = function(place){
         $location.path(place);
     };
-    
-//    //scope functions
-//    $scope.doAThing = function(){
-//        //do something
-//    };
 
 }]);
 
